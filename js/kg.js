e952fb69d06d4abbf3ed3750c45f148d1f9cee89
Andre Henn <henn@terrestris.de>
2017-06-25 22:45:25 +0200

    WIP gallery

--- js/kg.js
+++ js/kg.js
@@ -26,20 +26,6 @@
     $('#prinzenCarousel').carousel({
         interval: undefined
     })
-    $('.fdi-Carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        }
-        else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
 
     // Carousel
     $('#myCarousel').carousel({
