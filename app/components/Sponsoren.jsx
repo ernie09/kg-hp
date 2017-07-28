b39ebb863bcfbac00611e66a10212025b56eab88
Andre Henn <henn@terrestris.de>
2017-07-28 11:28:24 +0200

    adds sponsors page and fixes layout issures

--- app/components/Sponsoren.jsx
+++ app/components/Sponsoren.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Header from './Header.jsx';
-//import PropTypes from 'prop-types';
+
+import {Grid, Row, Col, Thumbnail} from 'react-bootstrap';
 
 class Sponsoren extends Component {
 
@@ -14,14 +15,58 @@ class Sponsoren extends Component {
     return (
       <div>
         <Header titleText={'Sponsoren'}/>
-        TODO
+        <br/>
+        <Grid>
+          <Row>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 1</h3>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 2</h3>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 3</h3>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 4</h3>
+              </Thumbnail>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 5</h3>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 6</h3>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 7</h3>
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={3}>
+              <Thumbnail href="#" alt="300x300" src="http://via.placeholder.com/300x300">
+                <h3>Firma 8</h3>
+              </Thumbnail>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
 }
 
-Sponsoren.propTypes = {
-
-};
+Sponsoren.propTypes = {};
 
 export default Sponsoren;
--- app/components/Vorstand.jsx
+++ app/components/Vorstand.jsx
@@ -17,21 +17,21 @@ class Vorstand extends Component {
         <section id="vorstand">
           <div className="container">
             <div className="row">
-              <div className="col-sm-4">
+              <div className="col-md-4">
                 <div className="team-member">
                   <img src="img/vorstand/1.png" className="img-responsive img-circle" alt=""/>
                   <h4>Andreas Faust</h4>
                   <p>1. Vorsitzender</p>
                 </div>
               </div>
-              <div className="col-sm-4">
+              <div className="col-md-4">
                 <div className="team-member">
                   <img src="img/vorstand/2.png" className="img-responsive img-circle" alt=""/>
                   <h4>Oliver Meier-Ronfeld</h4>
                   <p>Pr&auml;sident</p>
                 </div>
               </div>
-              <div className="col-sm-4">
+              <div className="col-md-4">
                 <div className="team-member">
                   <img src="img/vorstand/3.png" className="img-responsive img-circle" alt=""/>
                   <h4>Andreas Engel</h4>
@@ -40,14 +40,14 @@ class Vorstand extends Component {
               </div>
             </div>
             <div className="row">
-              <div className="col-sm-6">
+              <div className="col-md-6">
                 <div className="team-member">
                   <img src="img/vorstand/4.png" className="img-responsive img-circle" alt=""/>
                   <h4>Tobias Jost</h4>
                   <p>2. Vorsitzender</p>
                 </div>
               </div>
-              <div className="col-sm-6">
+              <div className="col-md-6">
                 <div className="team-member">
                   <img src="img/vorstand/5.png" className="img-responsive img-circle" alt=""/>
                   <h4>Claudia Wagner</h4>
